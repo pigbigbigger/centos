83271b5312384979ef2974ef26922dfe02b4677c
pigbigbigger <89382637+pigbigbigger@users.noreply.github.com>
2022-02-23 16:57:13 +0800

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -4,5 +4,5 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/p' ~/.zshrc
-$ sed -i '1i\export PATH=$HOME/bin:/usr/local/bin:/usr/bin:$PATH' ~/.zshrc
+sed -i '1i\export PATH=$HOME/bin:/usr/local/bin:/usr/bin:$PATH' ~/.zshrc
 source ~/.zshrc   
