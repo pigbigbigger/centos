c3aee86fc21911d969928edf842c92666bc24250
pigbigbigger <89382637+pigbigbigger@users.noreply.github.com>
2022-02-23 16:56:49 +0800

    Update init_centos.sh

--- init_centos.sh
+++ init_centos.sh
@@ -38,7 +38,8 @@ yum install zsh -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/p' ~/.zshrc 
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/p' ~/.zshrc
+sed -i '1i\export PATH=$HOME/bin:/usr/local/bin:/usr/bin:$PATH' ~/.zshrc
 source ~/.zshrc   
 chsh -s /bin/zsh
 
